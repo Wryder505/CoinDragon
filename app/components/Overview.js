d6c9da8546f5c99263ba82e6b6d712a2bb3e35b5
WRyder505 <wryder505@gmail.com>
2025-07-20 17:04:15 -0600

    Initial Commit

---
+++ app/components/Overview.js
@@ -0,0 +1,101 @@
+import { useState } from 'react'
+
+import Image from 'next/image'
+
+// Components
+import Account from './Account'
+import Add from './Add'
+
+// Images
+import up from '../assets/up.svg'
+import down from '../assets/down.svg'
+import add from '../assets/add.svg'
+
+const Overview = ({ account, setAccount, markets, trackedTokens, setTrackedTokens }) => {
+
+	const [isAccountModalOpen, setIsAccountModalOpen] = useState(false)
+	const [isAddTokenModalOpen, setIsAddTokenModalOpen] = useState(false)
+
+	const accountModalHandler = () => {
+		setIsAccountModalOpen(true)
+	}
+
+		const tokenModalHandler = () => {
+			if (account) {
+				setIsAddTokenModalOpen(true)
+			} else {
+				setIsAccountModalOpen(true)
+			}
+		}
+
+	return (
+		<div className="overview">
+			<div className="overview__account">
+				<h3>Account</h3>
+				{ account ?(
+					<p>{account.slice(0,6) + "..." + account.slice(-4)}</p>
+				) : (
+					<button onClick={accountModalHandler}>
+						<Image 
+							src={add}
+							width={20}
+							height={20}
+							alt="Set account"
+						/> 
+					</button>
+				)}
+
+			</div>
+
+			<div className="overview__tracked">
+				<h3>Assets Tracked</h3>
+				<p>{trackedTokens.length}</p>
+				<button onClick={tokenModalHandler} >
+					<Image 
+						src={add}
+						width={20}
+						height={20}
+						alt="Add token"
+					/>
+				</button>
+			</div>
+
+			<div className="overview__total">
+				<h3>Total Value</h3>
+				<p>$0.00</p>
+			</div>
+
+			<div className="overview__change">
+				<h3>% Change</h3>
+				<p>
+					<Image
+						src={up}
+						width={20}
+						height={20}
+						alt="Change direction"
+					/>
+					<span className="green">0.00%</span>
+				</p>
+			</div>
+
+			{isAccountModalOpen &&
+				<Account
+					setIsAccountModalOpen={setIsAccountModalOpen}
+					setAccount={setAccount}
+				/>
+			}
+
+			{isAddTokenModalOpen &&
+				<Add
+					setIsAddTokenModalOpen={setIsAddTokenModalOpen}
+					markets={markets}
+					trackedTokens={trackedTokens}
+					setTrackedTokens={setTrackedTokens}
+				/>
+			}
+
+		</div>
+	);
+}
+
+export default Overview;
--- app/page.js
+++ app/page.js
@@ -1,94 +1,34 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
+
+import { useState } from 'react'
+
+// Components
+import Overview from './components/Overview'
+
+// Snapshot Data
+import marketSnapshot from './snapshots/markets.json'
 
 export default function Home() {
+  const [account, setAccount] = useState(null)
+  const [trackedTokens, setTrackedTokens] = useState([])
+
+  const [markets, setMarkets] = useState(marketSnapshot)
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main>
+      <h2>Portfolio Overview</h2>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <Overview
+        account={account}
+        setAccount={setAccount}
+        markets={markets}
+        trackedTokens={trackedTokens}
+        setTrackedTokens={setTrackedTokens}
+      />
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      <div className="details">
+        <div className="divider"></div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   )
